0d3e8611ccf558d681309515d51f78aed6dd4599
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-12-27 11:25:36 +0300

    first commit

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from'next/link'
+function HeroBanner() {
+  return (
+      <div
+          className='hero-banner-container'>
+          <div><p className='beats-solo'> SMALL TEXT</p>
+              <h3>MID TEXT</h3>
+              <img src="" alt="headphones" className='hero-banner-img' />
+              <div>
+                  <Link href='product/ID'>
+                      <button type='button'> BUTTON TEXT</button>
+                  </Link>
+                  <div className='desc'>
+                      <h5>DESCRPTION</h5>
+                      <p>DSCRIPTION</p>
+                  </div>
+              </div>
+          </div>
+
+      </div>
+  )
+}
+
+export default HeroBanner
---
+++ sanity-ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'c2pyx2yn',
+    dataset: 'production'
+  }
+})
