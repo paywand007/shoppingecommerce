0e60349e71fafa6c7a3b9374ddcdb10b919d5b9e
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2023-02-26 00:06:59 +0300

    just commite

--- sanity-ecommerce/schemas/index.js
+++ sanity-ecommerce/schemas/index.js
@@ -7,5 +7,6 @@ import dastidw from './dastidw'
 import gaming from './gaming'
 import tvStands from './tvStands'
  import headsetAirbuds from './headsetAirbuds'
-export const schemaTypes = [product, banner, dastidw, mobile,headsetAirbuds, gaming,tvStands, computersHardwares]
+import photograph from './photograph'
+export const schemaTypes = [product, banner, dastidw, mobile,headsetAirbuds, gaming,tvStands,photograph, computersHardwares]
 
